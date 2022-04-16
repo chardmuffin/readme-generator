4d1a6b113cdbc03500f2aa7afc6cbd1d88af8ff7
Richard Huffman <richardhuffman96@gmail.com>
2022-04-15 22:28:34 -0400

    added real questions, generates a super basic README.md

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
-# Professional README Generator Starter Code
+# TEST
+## Description
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+
+
+## Usage
+
+
+
+## License
+
+MIT
+
+## Contributing
+
+
+
+## Tests
+
+
+
+## Questions
+
+I am on GitHub at [https://github.com/](https://github.com/)
+
+
+I can also be reached by email at [](mailto:)
--- index.js
+++ index.js
@@ -5,49 +5,111 @@ const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [
     {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name? (Required)',
-      validate: nameInput => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log('Please enter your name!');
-          return false;
-        }
-      }
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+              return true;
+            } else {
+              console.log('Please enter a title for your project!');
+              return false;
+            }
+          }
     },
     {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username (Required)',
-      validate: githubInput => {
-        if (githubInput) {
-          return true;
-        } else {
-          console.log('Please enter your GitHub username!');
-          return false;
-        }
-      }
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description explaining the what, why, and how of your project.',
     },
     {
-      type: 'confirm',
-      name: 'confirmAbout',
-      message: 'Would you like to enter some information about yourself for an "About" section?',
-      default: true
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
     },
     {
-      type: 'input',
-      name: 'about',
-      message: 'Provide some information about yourself:',
-      when: ({ confirmAbout }) => confirmAbout
-    }];
+        type: 'input',
+        name: 'usage',
+        message: 'Provide step-by-step instructions for use.',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select a license',
+        choices: ['MIT', 'ISC', 'Apache', 'GNU GPLv3', 'BSD', 'None']
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'List any collaborators, third-party assets that require attribution, and links to tutorials used, if any.',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide examples on how to run any tests.',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username',
+    }
+    ,
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email',
+    }
+];
 
-// TODO: Create a function to write README file
+// this function writes the README file
 function writeToFile(fileName, data) {
 
+    //TODO put data in fileContent in format;
+    console.log(data);
+    const fileContent = `# ${data.title}
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## License
+
+${data.license}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+I am on GitHub at [https://github.com/${data.github}](https://github.com/${data.github})
+
+
+I can also be reached by email at [${data.email}](mailto:${data.email})
+`;
+
     return new Promise((resolve, reject) => {
-        fs.writeFile(fileName, data, err => {
+        fs.writeFile(fileName, fileContent, err => {
             if (err) {
                 reject(err);
                 return;
@@ -61,17 +123,13 @@ function writeToFile(fileName, data) {
     });
 };
 
-// TODO: Create a function to initialize app
+// initialize app, calls functions
 function init() {
+    console.log("Any information not entered will be left blank in the generated README.me file")
     inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?'
-        }
-    ])
-    .then(answers => console.log(answers));
+    .prompt(questions)
+    //.then(answers => console.log(answers))
+    .then(answers => writeToFile('README.md', answers));
 }
 
 // Function call to initialize app
